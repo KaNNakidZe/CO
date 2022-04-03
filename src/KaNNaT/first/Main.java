7f7fdfec86e031102dddf5ce523ecd4400d94e54
KaNNaT <KaNNaT13@yandex.ru>
2022-04-03 20:26:56 +0300

    CO

---
+++ src/KaNNaT/first/Main.java
@@ -0,0 +1,15 @@
+package KaNNaT.first;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int age = 35;
+        if (age >=18) {
+            System.out.println("Ты можешь водить");
+        } else {
+
+        }
+
+        System.out.println("Ты не можешь водить");
+    }
+    }
